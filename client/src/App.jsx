96f93240dabf509020f16a3c29528df55aa60692
selemskr <selemskr@gmail.com>
2023-07-23 13:12:40 +0200

    ✨ Adding Login and Register with jwt

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Routes from "./Routes";
+import { UserContext, UserContextProvider } from "./UserContext";
+import axios from "axios";
+function App() {
+  axios.defaults.baseURL = "http://localhost:4040";
+  axios.defaults.withCredentials = true;
+  return (
+    <UserContextProvider>
+      <Routes />
+    </UserContextProvider>
+  );
+}
+
+export default App;
